e3cef1b20261716bbe2df760efb76fa89fddb859
Nalitha Liyanage <nalitha770@gmail.com>
2015-04-23 10:59:24 -0700

    m 16

--- index.php
+++ index.php
@@ -1,20 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Simple To-DO List </title>
+	<title>Simple To-Do List</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-</head>
 <body>
-	<div class="wrap">
-	<div class="task-list">
-	 <ul>
-	 	<?php require("includes/connect.php"); ?>
-	 </ul>
-	</div>
-	<form class="add-new-task" autocomplete="off">
-		<input type="text" name="new-task" placeholder="Add new item..."/>
-	</form>
-	</div>
+	<div class="wrap"> 
+	 	<div class="task-list">
+			<ul>
+				<?php require("includes/connect.php"); 
+				$mysqli = new mysqli('localhost', 'root', 'root', 'task');
+				$query = "SELECT = FROM tasks ORDER BY date ASC, time ASC";
+				if($result = $mysqli->query($query)){
+					$numrows = $result->num_rows;
+					if ($numrows>0){
+						while($row = $result->fetch_assoc()){
+							$task_id = $row['id'];
+							$task_name = $row["task"];
+
+							echo '<li>
+							<span>'.$task_name. '</span>
+							<img id="'.$task_id.'"class="delete-button" width="10px" src="images/close.svg"/>
+							</li>';
+						}
+					}
+				}
+
+				?>
+
+
+			</ul>
+		</div>
+		<form class="add-new-task" autocomplete="off">
+			<input type="text" name="new-task" placeholder="Add new item..."/>
+		</form>
+		</div>
 </body>
 <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 <script>
@@ -33,5 +52,17 @@
 			return false;
 		});
 	}
+
+	$('.delete-button').click(function(){
+		var current_element = $(this);
+		var task_id = $(this).attr('id');
+
+		$.post('includes/delete-task.php', {id: task_id}, function(){
+		current_element.parent().fadeOut("fast", function(){
+			$(this).remove();
+		});
+	});
+});
 	</script>
+
 </html>
